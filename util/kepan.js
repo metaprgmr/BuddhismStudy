7d6c01aa4728f9bbd6b69dc8ea34adafdcd3dda8
James Huang <metaprgmr@gmail.com>
2023-11-12 10:31:40 -0800

    refactor and text mods

--- util/kepan.js
+++ util/kepan.js
@@ -26,95 +26,128 @@ function getPartInfo(part) {
   return ret;
 }
 
+class KePanLine {
+  constructor(line) {
+    this.rawVerseNum = line.substring(2,6).trim();
+    this.volNum = line.substring(2,3).trim();
+    this.verseNum = line.substring(3,6).trim();
+    this.text = line.substring(6);
+    var ln = this.text, idx = ln.indexOf('*');
+    if (idx < 0) {
+      idx = ln.indexOf('+');
+      if (idx > 0) this.isKeySecondary = true;
+    }
+    if (idx > 0) {
+      this.isKey = true;
+      var idx1 = ln.lastIndexOf('　', idx-1);
+      this.text = ln.substring(0, idx1+1) + '<font class="keyLine">' + ln.substring(idx1+1, idx) + '</font>' + ln.substring(idx+1);
+    }
+  }
+  getVerseIDDisp(suggestedRawVerseNum) {
+    var v = this.volNum;
+    var n = this.verseNum;
+    if (!v && suggestedRawVerseNum) {
+      v = suggestedRawVerseNum[0];
+      n = suggestedRawVerseNum.substring(1);
+    }
+    if (!v || !n) return '';
+    switch (v) {
+    case 'a': case 'A': v = '　一'; break;  case 'b': case 'B': v = '　二'; break;
+    case 'c': case 'C': v = '　三'; break;  case 'd': case 'D': v = '　四'; break;
+    case 'e': case 'E': v = '　五'; break;  case 'f': case 'F': v = '　六'; break;
+    case 'g': case 'G': v = '　七'; break;  case 'h': case 'H': v = '　八'; break;
+    case 'i': case 'I': v = '　九'; break;  case 'j': case 'J': v = '十　'; break;
+    case 'k': case 'K': v = '十一'; break;  case 'l': case 'L': v = '十二'; break;
+    case 'm': case 'M': v = '十三'; break;  case 'n': case 'N': v = '十四'; break;
+    case 'o': case 'O': v = '十五'; break;  case 'p': case 'P': v = '十六'; break;
+    case 'q': case 'Q': v = '十七'; break;  case 'r': case 'R': v = '十八'; break;
+    case 's': case 'S': v = '十九'; break;  case 't': case 'T': v = '廿　'; break;
+    case 'u': case 'U': v = '廿一'; break;  case 'v': case 'V': v = '廿二'; break;
+    case 'w': case 'W': v = '廿三'; break;  case 'x': case 'X': v = '廿四'; break;
+    case 'y': case 'Y': v = '廿五'; break;  case 'z': case 'Z': v = '廿六'; break;
+    }
+    return v + '&nbsp;' + n;
+  }
+} // end of KePanLine.
+
+class KePanDoc {
+  constructor(kpdocText) {
+    this.lines = [];
+    this.maxlen = 40;
+    if (kpdocText) this.addAll(kpdocText);
+  }
+  clearall() { this.lines = [] }
+  addAll(kpdocText) {
+    var a = kpdocText.split('\n');
+    for (var i in a) {
+      var ln = a[i];
+      if (ln.length > 0 && !ln.startsWith('//'))
+        this.add(new KePanLine(ln));
+    }
+  }
+  add(kpl) {
+    if (!kpl) return;
+    if (kpl.length > this.maxlen) this.maxlen = kpl.length - 6;
+    this.lines.push(kpl);
+  }
+  getRawVerseID(lineIdx, force) { // 0-based
+    var kpn = this.lines[lineIdx].rawVerseNum;
+    if (kpn !== '' || !force) return kpn;
+    // look for the next available verse ID
+    for (var i=lineIdx+1; i<this.lines.length; ++i) {
+      kpn = this.lines[i].rawVerseNum;
+      if (kpn !== '') return kpn;
+    }
+    return '';
+  }
+  getVerseIDDisp(lineIdx, force) { // 0-based
+    return this.lines[lineIdx].getVerseIDDisp( this.getRawVerseID(lineIdx, force) );
+  }
+  writeAsTRs(buf, terseLevel, forceVerseIDs) {
+    var td1 = '<tr><td valign="top" nowrap style="font-size:12px; background-color:#e8e8e8; border-bottom:1px #fff solid">';
+    var td2 = '<td valign="top" align="right" nowrap style="font-size:12px; color:gray">';
+    var lastIDDisp = null;
+    for (var i=0; i<this.lines.length; ++i) {
+      var ln = this.lines[i];
+      var anchor = ''; // TODO: use?
+      if (!ln.isKey && terseLevel) continue;
+      if (terseLevel === 2 && ln.isKeySecondary) continue;
+      var idDisp = this.getVerseIDDisp(i, terseLevel && forceVerseIDs);
+      if (idDisp != lastIDDisp)
+        lastIDDisp = idDisp;
+      else
+        idDisp = '';
+      buf.w(td1, '&nbsp;', anchor, ln.text, '&nbsp;</td>', td2, idDisp, '</td></tr>');
+    }
+  }
+} // end of KePanDoc.
+
 var lastDivId;
 
 // A global "parts" variable is assumed.
-function write楞嚴經圓瑛大師科判(divId, part, terse) {
-  if (divId) lastDivId = divId; else divId = lastDivId;
-  var partNum, buf = new Buffer();
-  switch (part) {
-  case 1: case '1': part = 1; partNum = '一'; break;
-  case 2: case '2': part = 2; partNum = '二'; break;
-  case 3: case '3': part = 3; partNum = '三'; break;
-  case 4: case '4': part = 4; partNum = '四'; break;
-  case 5: case '5': part = 5; partNum = '五'; break;
-  case 6: case '6': part = 6; partNum = '六'; break;
-  default:  part = 0; partNum = ''; break;
+// terseLevel: 1 - 精簡版
+//             2 - 極簡版
+//             ? - 詳細版
+function write楞嚴經圓瑛大師科判(divId, part, terseLevel) {
+  var verseLink    = '<a href="YuanYing科判Verses.html">經文參考</a>',
+      level0Link   = '<a href="javascript:write楞嚴經圓瑛大師科判(null,' + (part || '\'\'') + ',0)">詳細版</a>&nbsp;&nbsp;',
+      level1Link   = '<a href="javascript:write楞嚴經圓瑛大師科判(null,' + (part || '\'\'') + ',1)">精簡版</a>&nbsp;&nbsp;',
+      level2Link   = '<a href="javascript:write楞嚴經圓瑛大師科判(null,' + (part || '\'\'') + ',2)">極簡版</a>&nbsp;&nbsp;',
+      level0Active = '<span style="font-weight:bold; background-color:yellow">詳細版</span>&nbsp;&nbsp;',
+      level1Active = '<span style="font-weight:bold; background-color:yellow">精簡版</span>&nbsp;&nbsp;',
+      level2Active = '<span style="font-weight:bold; background-color:yellow">極簡版</span>&nbsp;&nbsp;';
+  var buf = new Buffer('<h2>圓瑛大師《楞嚴經講義》</h2>');
+  buf.w('<table border="0" cellspacing="0px" cellpadding="0" style="min-width:500px"><caption>');
+  switch (terseLevel) {
+  default: buf.w(level0Active, level1Link, level2Link, verseLink); break;
+  case 1:  buf.w(level0Link, level1Active, level2Link, verseLink); break;
+  case 2:  buf.w(level0Link, level1Link, level2Active, verseLink); break;
   }
-
-  buf.w('<h2>圓瑛大師《楞嚴經講義》');
-  if (part > 0) buf.w('<a href="?part=&terse=', terse, '">科判</a>'); else buf.w('科判');
-  buf.w('</h2>');
-  var partInfo = getPartInfo(part);
-
-  buf.w('<table border="0" cellspacing="0px" cellpadding="0" style="min-width:500px">');
-  buf.w('<caption><a href="javascript:write楞嚴經圓瑛大師科判(null,', part || '\'\'', ',', terse ? '0' : '1', ')">', terse ? '詳細版' : '精簡版', '</a>');
-  if (!terse) buf.w('&nbsp;&nbsp;<a href="YuanYing科判Verses.html">經文參考</a>');
   buf.w('</caption>');
-  var kepan = parts[part];
-  var i, ln, a = kepan.split('\n'), maxlen = 50;
-  for (i in a) {
-    ln = a[i];
-    if (ln.startsWith('//')) continue;
-    if (ln.length > maxlen) maxlen = ln.length;
-  }
-  for (i in a) {
-    ln = a[i];
-    var anchor = '';
-    if (ln.length === 0 || ln.startsWith('//')) continue;
-    var verseNum = '';
-    if (!terse) {
-      var volNum = ln.substring(2,3);
-      verseNum = ln.substring(3,6);
-      switch (volNum) {
-      case 'A': verseNum = '　一&nbsp;' + verseNum; break;
-      case 'B': verseNum = '　二&nbsp;' + verseNum; break;
-      case 'C': verseNum = '　三&nbsp;' + verseNum; break;
-      case 'D': verseNum = '　四&nbsp;' + verseNum; break;
-      case 'E': verseNum = '　五&nbsp;' + verseNum; break;
-      case 'F': verseNum = '　六&nbsp;' + verseNum; break;
-      case 'G': verseNum = '　七&nbsp;' + verseNum; break;
-      case 'H': verseNum = '　八&nbsp;' + verseNum; break;
-      case 'I': verseNum = '　九&nbsp;' + verseNum; break;
-      case 'J': verseNum = '十　&nbsp;' + verseNum; break;
-      case 'K': verseNum = '十一&nbsp;' + verseNum; break;
-      case 'L': verseNum = '十二&nbsp;' + verseNum; break;
-      case 'M': verseNum = '十三&nbsp;' + verseNum; break;
-      case 'N': verseNum = '十四&nbsp;' + verseNum; break;
-      case 'O': verseNum = '十五&nbsp;' + verseNum; break;
-      case 'P': verseNum = '十六&nbsp;' + verseNum; break;
-      case 'Q': verseNum = '十七&nbsp;' + verseNum; break;
-      case 'R': verseNum = '十八&nbsp;' + verseNum; break;
-      case 'S': verseNum = '十九&nbsp;' + verseNum; break;
-      case 'T': verseNum = '廿　&nbsp;' + verseNum; break;
-      case 'U': verseNum = '廿一&nbsp;' + verseNum; break;
-      case 'V': verseNum = '廿二&nbsp;' + verseNum; break;
-      case 'W': verseNum = '廿三&nbsp;' + verseNum; break;
-      case 'X': verseNum = '廿四&nbsp;' + verseNum; break;
-      case 'Y': verseNum = '廿五&nbsp;' + verseNum; break;
-      case 'Z': verseNum = '廿六&nbsp;' + verseNum; break;
-      }
-    }
-    var kepanHint = '';
-    var idx = ln.lastIndexOf('：');
-//    if (idx > 0) kepanHint = ln.substring(idx+1).trim(); // if kepan font too small, use hint to help
-    var kepanLine = ln.substring(6);
-    idx = kepanLine.indexOf('*');
-    var isKey = idx > 0;
-    if (isKey) {
-      var idx1 = kepanLine.lastIndexOf('　', idx) + 1;
-      ln = kepanLine;
-      kepanLine = ln.substring(0, idx1) + '<font class="keyLine">' + ln.substring(idx1, idx) + '</font>' + ln.substring(idx+1);
-    }
-    else if (terse) continue;
-    buf.w('<tr><td valign="top" nowrap style="font-size:12px; background-color:#e8e8e8; border-bottom:1px #fff solid">', anchor,
-          '<span title="', kepanHint, '">', kepanLine, '</span>',
-          '</td>');
-    if (!terse)
-      buf.w('<td valign="top" align="right" nowrap style="font-size:12px; color:gray; ">&nbsp;', verseNum, '</td>');
-    buf.w('</tr>');
-  }
+  new KePanDoc(parts[0]).writeAsTRs(buf, terseLevel, true);
   buf.w('</table>');
+
+  if (divId) lastDivId = divId; else divId = lastDivId;
   buf.render(divId);
 }
 
