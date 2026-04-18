c2320b0749f0c4ff87e4e6194eb9ca17650d0d5c
jhuang <jhuang@jhuangs-Mac-mini.local>
2026-04-17 20:16:26 -0700

    minor

--- BFNN/book/books9/9040/00.js
+++ BFNN/book/books9/9040/00.js
@@ -1,66 +1,65 @@
 SC.volNum = 0;
-SC.text = `
-${COL_START}
-${link(1)}
-${link(2)}
-${link(3)}
-${link(4)}
-${link(5)}
-${link(6)}
-${link(7)}
-${link(8)}
-${link(9)}
-${link(10)}
-${link(11)}
-${link(12)}
-${link(13)}
-${link(14)}
-${link(15)}
-${link(16)}
-${link(17)}
-${link(18)} *
-${link(19)} *
-${link(20)}
+SC.text =
+`${COL_START}
+${toclink(1,1)}
+${toclink(2,1)}
+${toclink(3,1)}
+${toclink(4,1)}
+${toclink(5,1)}
+${toclink(6,1)}
+${toclink(7,1)}
+${toclink(8,1)}
+${toclink(9,1)}
+${toclink(10,1)}
+${toclink(11,1)}
+${toclink(12,1)}
+${toclink(13,1)}
+${toclink(14,1)}
+${toclink(15,1)}
+${toclink(16,1)}
+${toclink(17,1)}
+${toclink(18,1)} *
+${toclink(19,1)} *
+${toclink(20,1)}
+${toclink(21,1)}
+${toclink(22,1)}
+${toclink(23,1)}
+${toclink(24,1)}
+${toclink(25,1)}
+${toclink(26,1)}
+${toclink(27,1)}
+${toclink(28,1)}
+${toclink(29,1)}
+${toclink(30,1)}
 ${COL_DIV}
-${link(21)}
-${link(22)}
-${link(23)}
-${link(24)}
-${link(25)}
-${link(26)}
-${link(27)}
-${link(28)}
-${link(29)}
-${link(30)}
-${link(31)}
-${link(32)}
-${link(33)}
-${link(34)}
-${link(35)}
-${link(36)}
-${link(37)}
-${link(38)}
-${link(39)}
-${link(40)}
-${COL_DIV}
-${link(41)}
-${link(42)}
-${link(43)}
-${link(44)}
-${link(45)}
-${link(46)}
-${link(47)}
-${link(48)}
-${link(49)}
-${link(50)}
-${link(51)}
-${link(52)}
-${link(53)}
+${toclink(31)}
+${toclink(32)}
+${toclink(33)}
+${toclink(34)}
+${toclink(35)}
+${toclink(36)}
+${toclink(37)}
+${toclink(38)}
+${toclink(39)}
+${toclink(40)}
+${toclink(41)}
+${toclink(42)}
+${toclink(43)}
+${toclink(44)}
+${toclink(45)}
+${toclink(46)}
+${toclink(47)}
+${toclink(48)}
+${toclink(49)}
+${toclink(50)}
+${toclink(51)}
+${toclink(52)}
+${toclink(53)}
 
 
 
-${link40('sk01')}
-${link40('sk02')}
-${link40('sk03')}
-${link40('sk04')}
+<ail>　　<a href="?vol=1">卷一</a>　</ail>${link40('sk01')}
+<ail>　　<a href="?vol=2&pin=sk02">卷二</a>　</ail>${link40('sk02')}
+<ail>　　<a href="?vol=3">卷三</a>　</ail>${link40('sk03')}
+<ail>　　<a href="?vol=4">卷四</a>　</ail>${link40('sk04')}
 ${COL_END}`;
